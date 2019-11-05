23affb529c9172929a951c9d4135105a090c6f39
crmc2 <crmc2@Dingdongskie>
2019-11-04 21:19:58 -0600

    Added fade-in javascript for education, workhistory, projects

--- Portfolio/js/scripts.js
+++ Portfolio/js/scripts.js
@@ -17,3 +17,23 @@ function closeFunc() {
 	document.body.style.overflow = "visible";
 }
 
+window.onload = function(){
+	var education = document.getElementById("education");
+	education.className = "fadeRightClass"
+}
+
+
+window.onscroll = function(){
+	var proficiencies = document.getElementById("proficiencies");
+	var workhistory = document.getElementById("workhistory");
+	var projects = document.getElementById("projects");
+	console.log("hey");
+	if(window.pageYOffset >= (proficiencies.offsetTop-window.innerHeight) + 250)
+		proficiencies.className = "fadeLeftClass";
+	
+	if(window.pageYOffset >= (workhistory.offsetTop+window.innerHeight))
+		workhistory.className = "fadeRightClass";
+	
+	if(window.pageYOffset >= (projects.offsetTop-window.innerHeight) + 350)
+		projects.className = "fadeLeftClass";	
+}
